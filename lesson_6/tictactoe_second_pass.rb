975a15b90e9c626c1eedbea6917e50c0d861b5fa
Norman José Pino Añez <normanpino@gmail.com>
2020-10-29 13:35:48 +0100

    Second commit of second pass at tictactoe game in lesson 6

--- lesson_6/tictactoe_second_pass.rb
+++ lesson_6/tictactoe_second_pass.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
                  [1, 5, 9], [3, 5, 7]]
@@ -10,37 +12,63 @@ def build_board
   (1..9).each_with_object({}) { |square, board| board[square] = MARKING_EMPTY }
 end
 
-def display_board(board, wins_player, wins_computer)
-  system("clear") || system("cls")
-  puts "=============="
-  puts "| SCOREBOARD"
-  puts "| PLAYER: #{wins_player}"
-  puts "| COMPUTER: #{wins_computer}"
-  puts "=============="
+def display_banner
+  puts " _   _        _               _              ".blue
+  puts "| | (_)      | |             | |             ".blue
+  puts "| |_ _  ___  | |_ __ _  ___  | |_ ___   ___  ".blue
+  puts "| __| |/ __| | __/ _` |/ __| | __/ _ \\ / _ \\ ".blue
+  puts "| |_| | (__  | || (_| | (__  | || (_) |  __/ ".blue
+  puts " \\__|_|\\___|  \\__\\__,_|\\___|  \\__\\___/ \\___| ".blue
   puts
-  puts "   |   |  "
-  puts " #{board[1]} | #{board[2]} | #{board[3]} "
-  puts "   |   |  "
-  puts "---+---+---"
-  puts "   |   |  "
-  puts " #{board[4]} | #{board[5]} | #{board[6]} "
-  puts "   |   |  "
-  puts "---+---+---"
-  puts "   |   |  "
-  puts " #{board[7]} | #{board[8]} | #{board[9]} "
-  puts "   |   |  "
+end
+
+def display_scoreboard(score)
+  puts "================".yellow
+  puts "|  SCOREBOARD  |".yellow
+  puts "|--------------|".yellow
+  puts "| PLAYER: #{score[:wins_player]}    |".yellow
+  puts "| COMPUTER: #{score[:wins_computer]}  |".yellow
+  puts "| TIES: #{score[:ties]}      |".yellow
+  puts "================".yellow
+  puts
+end
+
+def display_board_lines
+  puts "     |     |     ".light_black
+end
+
+def display_board(board, score)
+  system("clear") || system("cls")
+  display_banner
+  display_scoreboard(score)
+  display_board_lines
+  puts "  " + [board[1], board[2], board[3]].join("  |  ".light_black)
+#  puts "  #{board[1]}  " + "|".light_black +
+#       "  #{board[2]}  " + "|".light_black + "  #{board[3]} "
+  display_board_lines
+  puts "-----+-----+-----".light_black
+  puts "     |     |  ".light_black
+  puts "  #{board[4]}  " + "|".light_black +
+       "  #{board[5]}  " + "|".light_black + "  #{board[6]} "
+  puts "     |     |  ".light_black
+  puts "-----+-----+-----".light_black
+  puts "     |     |  ".light_black
+  puts "  #{board[7]}  " + "|".light_black +
+       "  #{board[8]}  " + "|".light_black + "  #{board[9]} "
+  puts "     |     |  ".light_black
+  puts
 end
 
 def empty_squares(board)
-  board.select { |square, value| value == MARKING_EMPTY }.keys
+  board.select { |_square, value| value == MARKING_EMPTY }.keys
 end
 
-def make_move(board, square, player_marker)
+def make_move!(board, square, player_marker)
   board[square] = player_marker
 end
 
 def board_full?(board)
-  board.none? { |square, marking| marking == MARKING_EMPTY }
+  board.none? { |_square, marking| marking == MARKING_EMPTY }
 end
 
 def win?(board, marking)
@@ -53,14 +81,14 @@ end
 
 def joinor(numbers)
   return numbers[0] if numbers.size == 1
-  return "#{numbers[0]} and #{numbers[1]}" if numbers.size == 2
-  numbers[0..-2].join(', ') << " and #{numbers[-1]}"
+  return "#{numbers[0]} or #{numbers[1]}" if numbers.size == 2
+  numbers[0..-2].join(', ') << " or #{numbers[-1]}"
 end
 
 def prompt_square(empty_squares)
   square = 0
   loop do
-    puts "Please select a box (#{joinor(empty_squares)}): "
+    puts "Select a box... #{joinor(empty_squares)}: "
     square = gets.chomp.to_i
     break if empty_squares.include?(square)
     puts "Sorry, that's not a valid square!"
@@ -68,58 +96,118 @@ def prompt_square(empty_squares)
   square
 end
 
-def play_game(board, wins_player, wins_computer)
-  display_board(board, wins_player, wins_computer)
+def risky_lines(board, marking)
+  lines = []
+  WINNING_LINES.each do |line|
+    count_marked = line.count { |square| board[square] == marking }
+    count_empty = line.count { |square| board[square] == MARKING_EMPTY }
+    lines << line if count_marked == 2 && count_empty == 1
+  end
+  lines
+end
+
+def assess_computer_options(board)
+  opportunities = risky_lines(board, MARKING_COMPUTER)
+  threats = risky_lines(board, MARKING_PLAYER)
+
+  if opportunities.size > 0
+    square = opportunities[0].select do |sq|
+      board[sq] == MARKING_EMPTY
+    end
+    square = square.first
+    puts "Opportunities(s) at #{opportunities}, beating you at #{square}!"
+  elsif threats.size > 0
+    square = threats[0].select do |sq|
+      board[sq] == MARKING_EMPTY
+    end
+    square = square.first
+    puts "Threat(s) at #{threats}, blocking you at #{square}!"
+  elsif board[5] == MARKING_EMPTY
+    square = 5
+    puts "Empty center, I'll take that thank you very much!"
+  else
+    square = empty_squares(board).sample
+    puts "No opportunities or threats... guess I'll move to #{square}"
+  end
+
+  sleep(4)
+  make_move!(board, square, MARKING_COMPUTER)
+end
+
+def process_player_move(board, score)
+  square = prompt_square(empty_squares(board))
+  make_move!(board, square, MARKING_PLAYER)
+  display_board(board, score)
+
+  if win?(board, MARKING_PLAYER)
+    puts "Congratulations, that's a win for you!"
+    score[:wins_player] += 1
+    score[:last_winner] = :player
+    :win
+  elsif board_full?(board)
+    puts "It's a tie!"
+    score[:ties] += 1
+    :tie
+  else
+    :nothing
+  end
+end
+
+def process_computer_move(board, score)
+  assess_computer_options(board)
+  display_board(board, score)
+
+  if win?(board, MARKING_COMPUTER)
+    puts "That's a win for me! Better luck next time."
+    score[:wins_computer] += 1
+    score[:last_winner] = :computer
+    :win
+  elsif board_full?(board)
+    puts "It's a tie!"
+    score[:ties] += 1
+    :tie
+  else
+    :nothing
+  end
+end
+
+def play_game(board, score)
+  display_board(board, score)
 
   loop do
-    square = prompt_square(empty_squares(board))
-    make_move(board, square, MARKING_PLAYER)
-    display_board(board, wins_player, wins_computer)
+    if score[:last_winner] == :player
+      result_first_move = process_player_move(board, score)
+      break unless result_first_move == :nothing
 
-    if win?(board, MARKING_PLAYER)
-      puts "Congratulations, you won!"
-      wins_player += 1
-      break
-    elsif board_full?(board)
-      puts "It's a tie!"
-      break
-    end
+      result = process_computer_move(board, score)
+      break unless result == :nothing
+    else
+      result = process_computer_move(board, score)
+      break unless result == :nothing
 
-    puts "Computer making move..."
-    sleep(1)
-    square = empty_squares(board).sample
-    make_move(board, square, MARKING_COMPUTER)
-    display_board(board, wins_player, wins_computer)
-
-    if win?(board, MARKING_COMPUTER)
-      puts "Sorry, the computer won!"
-      wins_computer += 1
-      break
-    elsif board_full?(board)
-      puts "It's a tie!"
-      break
+      result = process_player_move(board, score)
+      break unless result == :nothing
     end
   end
-  {wins_player: wins_player, wins_computer: wins_computer}
 end
 
 def prompt_game
-  wins_player = 0
-  wins_computer = 0
+  score = { wins_player: 0, wins_computer: 0, ties: 0, last_winner: :player }
+
   loop do
     board = build_board()
-    results = play_game(board, wins_player, wins_computer)
-    wins_player = results[:wins_player]
-    wins_computer = results[:wins_computer]
+    play_game(board, score)
 
-    if wins_player == GAMES_PER_MATCH
-      puts "Congratulations, you won the set at #{wins_player}/#{wins_computer}!"
+    if score[:wins_player] == GAMES_PER_MATCH
+      puts "Congratulations, you won the set at " \
+           " #{score[:wins_player]}/#{score[:wins_computer]}!"
       break
-    elsif wins_computer == GAMES_PER_MATCH
-      puts "Sorry, you lost the set at #{wins_player}/#{wins_computer}"
+    elsif score[:wins_computer] == GAMES_PER_MATCH
+      puts "Sorry, you lost the set at " \
+           "#{score[:wins_player]}/#{score[:wins_computer]}"
       break
     else
-      puts "preparing a new game..."
+      puts "I'm preparing a new board..."
       sleep(2)
     end
   end
@@ -135,3 +223,10 @@ def main
 end
 
 main()
+
+=begin debugging
+board = {1 => "X", 2 => " ", 3 => " ",
+         4=> "O", 5 => "O", 6 => " ",
+         7 => " ", 8 => " ", 9 => " "}
+process_computer_move(board)
+=end
