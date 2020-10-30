02bb82040f009cbe185641be86f3c8218debaaab
Norman José Pino Añez <normanpino@gmail.com>
2020-10-30 17:18:29 +0100

    Finish bonus features in tictac toe game (second pass) in lesson 6

--- lesson_6/tictactoe_second_pass.rb
+++ lesson_6/tictactoe_second_pass.rb
@@ -1,14 +1,17 @@
 require 'colorize'
+require 'pry'
 
+GAMES_NEEDED = 2
+CENTER_SQUARE = 5
+MARKING_EMPTY = ' '
+MARKING_PLAYER = 'X'
+MARKING_COMPUTER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
                  [1, 5, 9], [3, 5, 7]]
-MARKING_PLAYER = 'X'
-MARKING_COMPUTER = 'O'
-MARKING_EMPTY = ' '
-GAMES_PER_MATCH = 2
+FIRST_PLAYER = :choose
 
-def build_board
+def new_board
   (1..9).each_with_object({}) { |square, board| board[square] = MARKING_EMPTY }
 end
 
@@ -22,13 +25,13 @@ def display_banner
   puts
 end
 
-def display_scoreboard(score)
+def display_scoreboard(player_score, computer_score, ties)
   puts "================".yellow
   puts "|  SCOREBOARD  |".yellow
   puts "|--------------|".yellow
-  puts "| PLAYER: #{score[:wins_player]}    |".yellow
-  puts "| COMPUTER: #{score[:wins_computer]}  |".yellow
-  puts "| TIES: #{score[:ties]}      |".yellow
+  puts "| PLAYER: #{player_score}    |".yellow
+  puts "| COMPUTER: #{computer_score}  |".yellow
+  puts "| TIES: #{ties}      |".yellow
   puts "================".yellow
   puts
 end
@@ -51,13 +54,13 @@ def display_board_crossline
   puts "-----+-----+-----".light_black
 end
 
-def display(board, score)
+def display(game_state)
   system("clear") || system("cls")
-
   display_banner
-  display_scoreboard(score)
-  display_board(board)
-
+  display_scoreboard(game_state[:wins_player],
+                     game_state[:wins_computer],
+                     game_state[:ties])
+  display_board(game_state[:board])
   puts
 end
 
@@ -94,10 +97,10 @@ end
 def prompt_square(empty_squares)
   square = 0
   loop do
-    puts "Select a box... #{joinor(empty_squares)}: "
+    puts "Please select a square... #{joinor(empty_squares)}: "
     square = gets.chomp.to_i
     break if empty_squares.include?(square)
-    puts "Sorry, that's not a valid square!"
+    puts "Sorry, that is not a valid square!"
   end
   square
 end
@@ -112,109 +115,167 @@ def risky_lines(board, marking)
   lines
 end
 
-def assess_computer_options(board, opportunities, threats, center_empty)
+def show_lines_in_text(lines)
+  formatted = lines.map { |line| line.join('|') }
+  joinor(formatted)
+end
+
+def output_opportunities(opportunities, decision)
+  wording_opportunities =
+    opportunities.size > 1 ? "some opportunities" : "an opportunity"
+  wording_lines = show_lines_in_text(opportunities)
+  text = "Hmm... I see #{wording_opportunities} at #{wording_lines}, " \
+         " I'll mark #{decision}!"
+  prompt_thought_bubble(text)
+end
+
+def prompt_thought_bubble(text)
+  puts "COMPUTER: "
+  puts "oO( #{text} )".cyan
+end
+
+def output_threats(threats, decision)
+  wording_threats = threats.size > 1 ? "some threats" : "a threat"
+  wording_lines = show_lines_in_text(threats)
+  text = "Hmm... I see #{wording_threats} at #{wording_lines}, " \
+         "I'll block at #{decision}!"
+  prompt_thought_bubble(text)
+end
+
+def evaluate_options(board, opportunities, threats, center_empty)
   if opportunities.size > 0
     square = opportunities[0].select { |sq| board[sq] == MARKING_EMPTY }.first
-    puts "Opportunities(s) at #{opportunities}, beating you at #{square}!"
+    output_opportunities(opportunities, square)
   elsif threats.size > 0
     square = threats[0].select { |sq| board[sq] == MARKING_EMPTY }.first
-    puts "Threat(s) at #{threats}, blocking you at #{square}!"
+    output_threats(threats, square)
   elsif center_empty
-    square = 5
-    puts "Empty center, I'll take that thank you very much!"
+    square = CENTER_SQUARE
+    prompt_thought_bubble("What? Empty center square? " \
+                          "I'll take it thank you very much!")
   else
     square = random_square(board)
-    puts "No opportunities or threats... guess I'll move to #{square}"
+    puts "No opportunities or threats... guess I'll mark square #{square}"
   end
 
   square
 end
 
-def process_computer_move(board)
+def process_move(board)
   opportunities = risky_lines(board, MARKING_COMPUTER)
   threats = risky_lines(board, MARKING_PLAYER)
-  center_empty = board[5] == MARKING_EMPTY
+  center_empty = board[CENTER_SQUARE] == MARKING_EMPTY
 
-  square = assess_computer_options(board, opportunities, threats, center_empty)
+  square = evaluate_options(board, opportunities, threats, center_empty)
 
   sleep(3)
   make_move!(board, square, MARKING_COMPUTER)
 end
 
-def player_turn(board, score)
-  square = prompt_square(empty_squares(board))
-  make_move!(board, square, MARKING_PLAYER)
-  display(board, score)
+def computer_turn(game_state)
+  process_move(game_state[:board])
+  display(game_state)
 
-  if win?(board, MARKING_PLAYER)
+  if win?(game_state[:board], MARKING_COMPUTER)
+    puts "Computer wins! Better luck next time."
+    game_state[:wins_computer] += 1
+    :win
+  elsif board_full?(game_state[:board])
+    puts "It's a tie!"
+    game_state[:ties] += 1
+    :tie
+  else
+    :no_win_or_tie
+  end
+end
+
+def player_turn(game_state)
+  square = prompt_square(empty_squares(game_state[:board]))
+  make_move!(game_state[:board], square, MARKING_PLAYER)
+  display(game_state)
+
+  if win?(game_state[:board], MARKING_PLAYER)
     puts "Congratulations, that's a win for you!"
-    score[:wins_player] += 1
-    score[:last_winner] = :player
+    game_state[:wins_player] += 1
     :win
-  elsif board_full?(board)
+  elsif board_full?(game_state[:board])
     puts "It's a tie!"
-    score[:ties] += 1
+    game_state[:ties] += 1
     :tie
   else
-    :nothing
+    :no_win_or_tie
   end
 end
 
-def computer_turn(board, score)
-  process_computer_move(board)
-  display(board, score)
+def prompt_for_first_turn
+  answer = ""
+  loop do
+    puts "Would you like to go first? (y/n):"
+    answer = gets.chomp.downcase
 
-  if win?(board, MARKING_COMPUTER)
-    puts "That's a win for me! Better luck next time."
-    score[:wins_computer] += 1
-    score[:last_winner] = :computer
-    :win
-  elsif board_full?(board)
-    puts "It's a tie!"
-    score[:ties] += 1
-    :tie
+    break if ["n", "y"].include?(answer)
+
+    puts "Please enter a valid response (y/n):"
+  end
+  answer == "y" ? :player : :computer
+end
+
+def alternate_player(current_player)
+  current_player == :computer ? :player : :computer
+end
+
+def play_turn(state)
+  if state[:current_player] == :player
+    player_turn(state)
   else
-    :nothing
+    computer_turn(state)
   end
 end
 
-def play_game(board, score)
-  display(board, score)
-
+def play_turns(state)
   loop do
-    if score[:last_winner] == :player
-      break unless player_turn(board, score) == :nothing
-      break unless computer_turn(board, score) == :nothing
-    else
-      break unless computer_turn(board, score) == :nothing
-      break unless player_turn(board, score) == :nothing
-    end
+    break unless play_turn(state) == :no_win_or_tie
+    state[:current_player] = alternate_player(state[:current_player])
   end
 end
 
-def prompt_game
-  score = { wins_player: 0, wins_computer: 0, ties: 0, last_winner: :player }
+def play_round(state)
+  state[:board] = new_board()
+  display(state)
+  if state[:first_player] == :choose
+    state[:first_player] = prompt_for_first_turn
+  end
+  state[:current_player] = state[:first_player]
+  play_turns(state)
+end
 
+def play_game(state)
   loop do
-    board = build_board()
-    play_game(board, score)
+    play_round(state)
 
-    if score[:wins_player] == GAMES_PER_MATCH
+    if state[:wins_player] == GAMES_NEEDED
       puts "Congratulations, you won the game!"
       break
-    elsif score[:wins_computer] == GAMES_PER_MATCH
+    elsif state[:wins_computer] == GAMES_NEEDED
       puts "Sorry, you lost the game!"
       break
     else
-      puts "I'm preparing a new board..."
-      sleep(2)
+      puts "A new board is being prepared..."
+      sleep(3)
     end
   end
 end
 
 def main
   loop do
-    prompt_game
+    state = { board: {},
+              wins_player: 0,
+              wins_computer: 0,
+              ties: 0,
+              current_player: nil,
+              first_player: FIRST_PLAYER }
+
+    play_game(state)
     puts "Would you like to play again? (y/n)"
     break if gets.chomp.downcase == 'n'
   end
@@ -223,9 +284,17 @@ end
 
 main()
 
-=begin debugging
-board = {1 => "X", 2 => " ", 3 => " ",
-         4=> "O", 5 => "O", 6 => " ",
-         7 => " ", 8 => " ", 9 => " "}
-process_computer_move(board)
+=begin
+Bonus features:
+- joinor method for improved joins
+- keeps score
+- computer has AI defense
+- computer has AI offense
+- there are computer turn refinements
+  - computer takes chance to win over chance to block
+  - picks the center square if available and there
+    are no immediate threats or win opportunities
+  - constant determines who moves first
+    :choose, :player, :computer
+- improved game loop (keep track of current player)
 =end
