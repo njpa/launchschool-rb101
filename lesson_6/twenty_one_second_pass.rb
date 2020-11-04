2dc899f3327a6bab3de92933741e150b45aaed21
Norman José Pino Añez <normanpino@gmail.com>
2020-11-04 09:40:18 +0100

    Commit before starting refactor of twenty-one to use hash to store game state

--- lesson_6/twenty_one_second_pass.rb
+++ lesson_6/twenty_one_second_pass.rb
@@ -24,45 +24,84 @@ def deal!(deck, recipient)
   recipient << random_card
 end
 
-def display_player(players_cards)
-  puts "**********".cyan
-  puts "* PLAYER *".cyan
-  puts "**********".cyan
+def display_header
+  puts " _                      _".blue
+  puts "| |___      _____ _ __ | |_ _   _        ___  _ __   ___  ".blue
+  puts "| __\\ \\ /\\ / / _ \\ '_ \\| __| | | |_____ / _ \\| '_ \\ / _ \\ ".blue
+  puts "| |_ \\ V  V /  __/ | | | |_| |_| |_____| (_) | | | |  __/ ".blue
+  puts " \\__| \\_/\\_/ \\___|_| |_|\\__|\\__, |      \\___/|_| |_|\\___| ".blue
+  puts "                            |___/                         ".blue
+end
+
+def display_player(cards)
+  puts "****************".cyan
+  puts "* PLAYER  (#{total(cards)}) *".cyan
+  puts "****************".cyan
   puts
-  players_cards.each do |card|
+  cards.each do |card|
     puts "#{card[:face_value]} of #{card[:face]}".red
   end
 end
 
-def display_dealer(dealers_cards)
-  puts "**********".cyan
-  puts "* DEALER *".cyan
-  puts "**********".cyan
-  puts
-  dealers_cards.each do |card|
-    puts "#{card[:face_value]} of #{card[:face]}".red
+def display_dealer_header(cards, show_hidden_card)
+  if show_hidden_card
+    puts "***************".cyan
+    puts "* DEALER (#{total(cards)}) *".cyan
+    puts "***************".cyan
+  else
+    puts "**********".cyan
+    puts "* DEALER *".cyan
+    puts "**********".cyan
   end
 end
 
-def display(dealers_cards, players_cards)
+def display_dealer(cards, show_hidden_card)
+  display_dealer_header(cards, show_hidden_card)
+  puts
+  cards.each_with_index do |card, idx|
+    if idx == 0 && !show_hidden_card
+      puts "[hidden card]".red
+    else
+      puts "#{card[:face_value]} of #{card[:face]}".red
+    end
+  end
+end
+
+def display(dealers_cards, players_cards, show_dealer_card=false)
   system("clear") || system("cls")
+  display_header
+  puts
   display_player(players_cards)
   puts
-  display_dealer(dealers_cards)
+  display_dealer(dealers_cards, show_dealer_card)
   puts
   puts "________________________".light_black
   puts
 end
 
-def prompt_action
+def output_winner(dealer_total, player_total)
+  case dealer_total <=> player_total
+  when -1 then puts "PLAYER WINS!".green
+  when 1  then puts "DEALER WINS!".green
+  else         puts "IT'S A TIE!".green
+  end
+  puts
+end
+
+def output_status(message)
+  puts message.light_black
+  sleep(2)
+end
+
+def hit?
   answer = ""
   loop do
-    puts "Hit or stay? ('hit'/'stay')"
+    puts "Would you like to (h)it or (s)tay?"
     answer = gets.chomp.downcase
 
-    break if ["hit", "stay"].include?(answer)
+    break if ["h", "s"].include?(answer)
   end
-  answer == "hit" ? :hit : :stay
+  answer == "h"
 end
 
 def total(hand)
@@ -84,28 +123,18 @@ def number_aces(cards)
   cards.select { |card| card[:face_value] == "Ace" }.size
 end
 
-def player_turn(deck, dealers_cards, players_cards)
-  result = :stay
+def player_stays?(deck, dealers_cards, players_cards)
   loop do
-    if bust?(players_cards)
-      result = :bust
-      break
-    end
+    return false if bust?(players_cards)
 
-    if prompt_action == :hit
+    if hit?
       deal!(deck, players_cards)
     else
-      break
+      return true
     end
 
     display(dealers_cards, players_cards)
   end
-  result
-end
-
-def output_status(message)
-  puts message.light_black
-  sleep(2)
 end
 
 def dealer_turn(deck, dealers_cards, players_cards)
@@ -120,80 +149,62 @@ def dealer_turn(deck, dealers_cards, players_cards)
       break
     end
 
-    display(dealers_cards, players_cards)
+    display(dealers_cards, players_cards, true)
   end
   result
 end
 
-def output_winner(dealer_total, player_total)
-  case dealer_total <=> player_total
-  when -1 then puts "PLAYER WINS!".green
-  when 1  then puts "DEALER WINS!".green
-  else         puts "IT'S A TIE!".green
-  end
-  puts
-end
-
-def game(deck, players_cards, dealers_cards)
+def turns(deck, players_cards, dealers_cards, dealer_total, player_total)
   display(dealers_cards, players_cards)
-  player_result = player_turn(deck, dealers_cards, players_cards)
 
-  if player_result == :bust
-    puts "PLAYER BUSTS!".green
-  else
+  if player_stays?(deck, dealers_cards, players_cards)
+    display(dealers_cards, players_cards, true)
     output_status("Player stays...")
+    display(dealers_cards, players_cards, true)
+
     dealer_result = dealer_turn(deck, dealers_cards, players_cards)
-    display(dealers_cards, players_cards)
+    display(dealers_cards, players_cards, true)
 
     if dealer_result == :bust
       puts "DEALER BUSTS!".green
     else
       output_winner(total(dealers_cards), total(players_cards))
     end
+  else
+    puts "PLAYER BUSTS!".green
   end
 end
 
-def main
+def play_game
   deck = initialize_deck
   players_cards = []
   dealers_cards = []
+  dealer_total = 0
+  player_total = 0
   2.times { deal!(deck, players_cards) }
   2.times { deal!(deck, dealers_cards) }
-  game(deck, players_cards, dealers_cards)
+  turns(deck, players_cards, dealers_cards, dealer_total, player_total)
+end
+
+def another_game?
+  answer = ""
+
+  loop do
+    puts "Would you like to play another game? ('y'/'n')"
+    answer = gets.chomp.downcase
+    break if ["n", "y"].include?(answer)
+    puts "Please enter 'y' or 'n':"
+  end
+
+  answer == "y"
+end
+
+def main
+  loop do
+    play_game
+    break unless another_game?
+  end
+  puts "Thanks for playing Twenty-one!"
 end
 
 main
-
-=begin test for bust method
-hand = [
-        {face: "spades", face_value: "8", value: 8},
-        #{face: "spades", face_value: "Queen", value: 10},
-        {face: "clubs", face_value: "Ace", value: [1, 11]},
-        {face: "hearts", face_value: "2", value: 2},
-        {face: "diamonds", face_value: "Ace", value: [1, 11]}
-        #{face: "diamonds", face_value: "King", value: 10}
-       ]
-p bust?(hand)
-=end
-=begin
-DATA STRUCTURES
-===============
-`deck`:  A list of 52 hashes containing the keys:
-      'face', 'face_value', 'value',
-  For example:
-  [{face: 'spades', face_value': 'queen', value: 10},
-   {face: 'clubs', face_value': 'ace', value: [1, 11]},
-   {face: 'hearts', face_value': '4', value: 4},
-   {face: 'diamonds', face_value': 'king', value: 10},
-   ...
-  ]
-  Reasoning:
-  - we have to be able to tell the value
-  - the face value and the face is needed
-  - we have to pull cards randomly from this deck,
-    we need to differentiate between cards that have been handed out,
-`players_cards`:  an empty list, waiting to be populated with cards from
-                  the `deck` list
-`dealers_cards`:  an empty list, waiting to be populated with cards from
-                  the `deck` list
-=end
